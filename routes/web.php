32285d02e0abd04e831a603a4626eed3be4a7e69
bendjy33-cloud <bendjy33@gmail.com>
2026-07-21 00:06:58 -0400

    Temporary create admin route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@ use App\Models\User;
 Route::get('/check-users', function () {
     return User::select('id', 'name', 'email')->get();
 });
+
+use Illuminate\Support\Facades\Hash;
+
+Route::get('/create-admin', function () {
+    $user = User::firstOrCreate(
+        ['email' => 'admin@cledinfos.com'],
+        [
+            'name' => 'Admin',
+            'password' => Hash::make('Admin@12345'),
+        ]
+    );
+
+    return $user;
+});
