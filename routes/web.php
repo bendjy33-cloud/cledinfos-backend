1487155d4e8b913c8fb3e9870eadb7919e1b51e8
bendjy33-cloud <bendjy33@gmail.com>
2026-07-21 00:01:54 -0400

    Check users

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,9 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return 'TEST OK';
 });
+
+use App\Models\User;
+
+Route::get('/check-users', function () {
+    return User::select('id', 'name', 'email')->get();
+});
