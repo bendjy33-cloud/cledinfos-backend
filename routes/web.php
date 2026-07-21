01e77d32a40021cdc8374d823ff830f5f217a72e
bendjy33-cloud <bendjy33@gmail.com>
2026-07-21 00:13:50 -0400

    Remove temporary debug routes

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
-    return 'TEST OK';
-});
-
-use App\Models\User;
-
-Route::get('/check-users', function () {
-    return User::select('id', 'name', 'email')->get();
-});
-
-use Illuminate\Support\Facades\Hash;
-
-Route::get('/create-admin', function () {
-    $user = User::firstOrCreate(
-        ['email' => 'admin@cledinfos.com'],
-        [
-            'name' => 'Admin',
-            'password' => Hash::make('Admin@12345'),
-        ]
-    );
-
-    return $user;
-});
