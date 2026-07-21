f50f59df80d4606f8fea7d18aebb4e7f7a901ba4
bendjy33-cloud <bendjy33@gmail.com>
2026-07-21 01:16:34 -0400

    Configure CORS for Next.js frontend

--- config/corp.php
+++ config/corp.php
@@ -13,20 +13,26 @@ return [
         'sanctum/csrf-cookie',
     ],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => [
+        '*',
+    ],
 
     'allowed_origins' => [
         'http://localhost:3000',
+        'https://cledinfos.com',
+        'https://www.cledinfos.com',
     ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [
+        '*',
+    ],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
