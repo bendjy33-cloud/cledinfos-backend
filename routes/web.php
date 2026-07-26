33cb504a40fb7f9e359eb1b2cc17a9f3cef74a78
bendjy33-cloud <bendjy33@gmail.com>
2026-07-26 14:37:49 -0400

    test render route

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test-cloudinary', function () {
-    return [
-        'cloud_name' => config('cloudinary.cloud.cloud_name'),
-        'api_key' => config('cloudinary.cloud.api_key'),
-        'api_secret' => config('cloudinary.cloud.api_secret') ? 'OK' : 'MISSING',
-    ];
+Route::get('/test', function () {
+    return "Laravel OK";
 });
