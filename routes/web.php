482aa9bf74d9c8f094c67a71c53e03aa0b9569c7
bendjy33-cloud <bendjy33@gmail.com>
2026-07-26 13:01:15 -0400

    Add test route for post creation debugging

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Models\Post;
+
+Route::get('/test-post', function () {
+
+    return Post::create([
+        'title' => 'Test',
+        'slug' => 'test-' . time(),
+        'content' => 'Test',
+        'image' => 'https://example.com/test.jpg',
+        'category_id' => 1,
+        'author_id' => 1,
+        'keywords' => 'test',
+        'featured' => false,
+        'views' => 0,
+        'is_published' => true,
+        'published_at' => now(),
+    ]);
+
+});
