daf7ccbc8eb599be6968ace299f68a3f75610117
bendjy33-cloud <bendjy33@gmail.com>
2026-07-26 12:53:49 -0400

    Fix Cloudinary upload and add upload service

--- app/Filament/Resources/Posts/Pages/CreatePost.php
+++ app/Filament/Resources/Posts/Pages/CreatePost.php
@@ -4,15 +4,22 @@ namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
 
-    protected function handleRecordCreation(array $data): \Illuminate\Database\Eloquent\Model
+    protected function handleRecordCreation(array $data): Model
     {
-        \Log::info('POST DATA', $data);
-
-        return parent::handleRecordCreation($data);
+        try {
+            return parent::handleRecordCreation($data);
+        } catch (\Throwable $e) {
+            dd(
+                $e->getMessage(),
+                $e->getFile(),
+                $e->getLine()
+            );
+        }
     }
 }
