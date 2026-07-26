161f5e7176a4c42512f7fffeb7dd37ff0bf317d4
bendjy33-cloud <bendjy33@gmail.com>
2026-07-26 12:46:21 -0400

    Integrate Cloudinary image uploads with custom service

--- app/Filament/Resources/Posts/Pages/CreatePost.php
+++ app/Filament/Resources/Posts/Pages/CreatePost.php
@@ -8,4 +8,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+
+    protected function handleRecordCreation(array $data): \Illuminate\Database\Eloquent\Model
+    {
+        \Log::info('POST DATA', $data);
+
+        return parent::handleRecordCreation($data);
+    }
 }
