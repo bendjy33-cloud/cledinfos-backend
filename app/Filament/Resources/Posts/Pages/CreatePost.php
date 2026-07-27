ce27d02de3a3d981bf1b7a7224868c5913404dc7
bendjy33-cloud <bendjy33@gmail.com>
2026-07-27 16:59:26 -0400

    upload post image to cloudinary after create

--- app/Filament/Resources/Posts/Pages/CreatePost.php
+++ app/Filament/Resources/Posts/Pages/CreatePost.php
@@ -3,18 +3,61 @@
 namespace App\Filament\Resources\Posts\Pages;
 
 use App\Filament\Resources\Posts\PostResource;
+use App\Services\CloudinaryService;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
 
+
     protected function handleRecordCreation(array $data): Model
     {
         try {
-            return parent::handleRecordCreation($data);
+
+            $post = parent::handleRecordCreation($data);
+
+
+            if ($post->image) {
+
+                $path = storage_path(
+                    'app/public/' . $post->image
+                );
+
+
+                if (file_exists($path)) {
+
+                    $url = app(CloudinaryService::class)->upload(
+                        $path,
+                        'cledinfos/posts'
+                    );
+
+
+                    $post->updateQuietly([
+                        'image_url' => $url,
+                    ]);
+
+
+                    Log::info('Cloudinary upload success', [
+                        'url' => $url,
+                    ]);
+                }
+                else {
+
+                    Log::error('Image file not found', [
+                        'path' => $path,
+                    ]);
+                }
+            }
+
+
+            return $post;
+
+
         } catch (\Throwable $e) {
+
             dd(
                 $e->getMessage(),
                 $e->getFile(),
