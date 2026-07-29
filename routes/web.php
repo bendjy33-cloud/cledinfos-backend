8c6a4f9cc6e565e469e666f0d0b47005e3b64ea9
bendjy33-cloud <bendjy33@gmail.com>
2026-07-28 23:46:56 -0400

    Add Cloudinary debug route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use App\Services\CloudinaryService;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +10,22 @@ Route::get('/', function () {
 
 Route::get('/test', function () {
     return "Laravel OK";
+});
+
+Route::get('/test-cloudinary', function () {
+
+    $post = Post::latest()->first();
+
+    $path = storage_path('app/public/' . $post->image);
+
+    return [
+        'image' => $post->image,
+        'image_url' => $post->image_url,
+        'path' => $path,
+        'exists' => file_exists($path),
+        'upload' => app(CloudinaryService::class)->upload(
+            $path,
+            'cledinfos/posts'
+        ),
+    ];
 });
