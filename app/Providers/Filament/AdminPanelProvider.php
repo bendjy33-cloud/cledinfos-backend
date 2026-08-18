44493469228258109c155fb7bf7c8877a7094beb
bendjy33-cloud <bendjy33@gmail.com>
2026-08-18 11:55:19 -0400

    Fix Filament production authentication

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -23,11 +23,12 @@ class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
-        return $panel
+         return $panel
             ->default()
             ->id('admin')
             ->path('admin')
             ->login()
+            ->authGuard('web')
             ->colors([
                 'primary' => Color::Amber,
             ])
