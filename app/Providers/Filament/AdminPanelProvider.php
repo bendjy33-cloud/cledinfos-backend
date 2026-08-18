35668494bfc073e2148e2b915ff0a1f990bf37fc
bendjy33-cloud <bendjy33@gmail.com>
2026-08-18 12:07:06 -0400

    Adm

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,6 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->authGuard('web')
             ->colors([
                 'primary' => Color::Amber,
             ])
